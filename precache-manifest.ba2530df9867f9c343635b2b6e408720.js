333e21dad980aa53651e68b73d882966cd78fc71
Vaddi Achyuth Kumar <va20079148@D-C02Q704KF8J2.local>
2019-10-07 19:51:33 +0530

    Updates

---
+++ precache-manifest.ba2530df9867f9c343635b2b6e408720.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd23c1d2d029698137a4de6a60eae9f5",
+    "url": "/recipeHouse/index.html"
+  },
+  {
+    "revision": "cf87551568760f1926cb",
+    "url": "/recipeHouse/static/css/2.c439d030.chunk.css"
+  },
+  {
+    "revision": "629328a6a03ec112621f",
+    "url": "/recipeHouse/static/css/main.70443dcc.chunk.css"
+  },
+  {
+    "revision": "cf87551568760f1926cb",
+    "url": "/recipeHouse/static/js/2.306156fd.chunk.js"
+  },
+  {
+    "revision": "629328a6a03ec112621f",
+    "url": "/recipeHouse/static/js/main.33ded240.chunk.js"
+  },
+  {
+    "revision": "4869e86b76abe8a06cfe",
+    "url": "/recipeHouse/static/js/runtime~main.5324dd90.js"
+  },
+  {
+    "revision": "9bb1d07fb83be6c158a43b79f1f9d6ba",
+    "url": "/recipeHouse/static/media/food_bg.9bb1d07f.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/recipeHouse/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/recipeHouse/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/recipeHouse/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/recipeHouse/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/recipeHouse/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
