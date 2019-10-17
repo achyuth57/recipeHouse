13d1e9a660e166a6680291bd2c03944d052b9985
Vaddi Achyuth Kumar <va20079148@D-C02Q704KF8J2.local>
2019-10-17 13:09:42 +0530

    Loading images

--- src/components/home/HomeComp.js
+++ src/components/home/HomeComp.js
@@ -18,10 +18,12 @@ class HomeComp extends Component {
     const data = API.posts("./recipes.json").getAll();
     data
       .then(res => {
-        this.setState({
-          data: res.data.recipes,
-          loading: false
-        });
+        setTimeout(() => {
+          this.setState({
+            data: res.data.recipes,
+            loading: false
+          });
+        }, 2000);
 
         //console.log(res.data.recipes);
       })
@@ -52,122 +54,125 @@ class HomeComp extends Component {
 
     return (
       <React.Fragment>
-        <div className="container bodyBg">
-          <div className="row">
-            {console.log("Render")}
-            <div className="col-md-12">
-              <h2>
-                Latest <b>Recipes</b>
-              </h2>
-              <div
-                id="myCarousel"
-                className="carousel slide"
-                data-ride="carousel"
-                data-interval="0"
-              >
-                {
-                  <ol className="carousel-indicators">
-                    {this.splitEvery(items, 4).map((data, index) => (
-                      <li
-                        key={index}
-                        data-target="#myCarousel"
-                        data-slide-to={index}
-                        className={index === 0 ? "active" : ""}
-                      />
-                    ))}
-                  </ol>
-                }
-                <div className="carousel-inner">
-                  {!loading
-                    ? this.splitEvery(items, 4).map((usersChunk, index) => (
-                        <div
+        {!loading ? (
+          <div className="container bodyBg">
+            <div className="row">
+              <div className="col-md-12">
+                <h2>
+                  Latest <b>Recipes</b>
+                </h2>
+
+                <div
+                  id="myCarousel"
+                  className="carousel slide"
+                  data-ride="carousel"
+                  data-interval="0"
+                >
+                  {
+                    <ol className="carousel-indicators">
+                      {this.splitEvery(items, 4).map((data, index) => (
+                        <li
                           key={index}
-                          className={`item carousel-item ${
-                            index === 0 ? "active" : ""
-                          } `}
-                        >
-                          <div className="row display-flex">
-                            {usersChunk.map((data, index) => {
-                              return (
-                                <div key={index} className="col-sm-3 col-xs-12">
-                                  <div className="thumb-wrapper">
-                                    <div className="img-box">
-                                      <img
-                                        height="200"
-                                        src={data.image_url}
-                                        className="img-responsive img-fluid"
-                                        alt={data.title}
-                                      />
+                          data-target="#myCarousel"
+                          data-slide-to={index}
+                          className={index === 0 ? "active" : ""}
+                        />
+                      ))}
+                    </ol>
+                  }
+                  <div className="carousel-inner">
+                    {this.splitEvery(items, 4).map((usersChunk, index) => (
+                      <div
+                        key={index}
+                        className={`item carousel-item ${
+                          index === 0 ? "active" : ""
+                        } `}
+                      >
+                        <div className="row display-flex">
+                          {usersChunk.map((data, index) => {
+                            return (
+                              <div key={index} className="col-sm-3 col-xs-12">
+                                <div className="thumb-wrapper">
+                                  <div className="img-box">
+                                    <img
+                                      height="200"
+                                      src={data.image_url}
+                                      className="img-responsive img-fluid"
+                                      alt={data.title}
+                                    />
+                                  </div>
+                                  <div className="thumb-content">
+                                    <h4>
+                                      {data.title.length > 30
+                                        ? data.title.substring(0, 40) + "..."
+                                        : data.title}
+                                    </h4>
+                                    <div className="star-rating">
+                                      <ul className="list-inline">
+                                        <li className="list-inline-item">
+                                          <i className="fa fa-star" />
+                                        </li>
+                                        <li className="list-inline-item">
+                                          <i className="fa fa-star" />
+                                        </li>
+                                        <li className="list-inline-item">
+                                          <i className="fa fa-star" />
+                                        </li>
+                                        <li className="list-inline-item">
+                                          <i className="fa fa-star" />
+                                        </li>
+                                        <li className="list-inline-item">
+                                          <i className="fa fa-star-o" />
+                                        </li>
+                                      </ul>
                                     </div>
-                                    <div className="thumb-content">
-                                      <h4>
-                                        {data.title.length > 30
-                                          ? data.title.substring(0, 40) + "..."
-                                          : data.title}
-                                      </h4>
-                                      <div className="star-rating">
-                                        <ul className="list-inline">
-                                          <li className="list-inline-item">
-                                            <i className="fa fa-star" />
-                                          </li>
-                                          <li className="list-inline-item">
-                                            <i className="fa fa-star" />
-                                          </li>
-                                          <li className="list-inline-item">
-                                            <i className="fa fa-star" />
-                                          </li>
-                                          <li className="list-inline-item">
-                                            <i className="fa fa-star" />
-                                          </li>
-                                          <li className="list-inline-item">
-                                            <i className="fa fa-star-o" />
-                                          </li>
-                                        </ul>
-                                      </div>
-                                      <p className="item-price">
-                                        {/* <strike>$400.00</strike> <b>$369.00</b> */}
-                                        Social Rank: {data.social_rank}
-                                      </p>
-                                      <Link
-                                        to={{
-                                          pathname: `/recipeView/${data.recipe_id}`,
-                                          state: {
-                                            prevPath: this.props.location
-                                              .pathname
-                                          }
-                                        }}
-                                        className="btn btn-primary"
-                                      >
-                                        View Recipe
-                                      </Link>
-                                    </div>
+                                    <p className="item-price">
+                                      {/* <strike>$400.00</strike> <b>$369.00</b> */}
+                                      Social Rank: {data.social_rank}
+                                    </p>
+                                    <Link
+                                      to={{
+                                        pathname: `/recipeView/${data.recipe_id}`,
+                                        state: {
+                                          prevPath: this.props.location.pathname
+                                        }
+                                      }}
+                                      className="btn btn-primary"
+                                    >
+                                      View Recipe
+                                    </Link>
                                   </div>
                                 </div>
-                              );
-                            })}
-                          </div>
+                              </div>
+                            );
+                          })}
                         </div>
-                      ))
-                    : "Loading..."}
+                      </div>
+                    ))}
+                  </div>
+                  <a
+                    className="carousel-control left carousel-control-prev"
+                    href="#myCarousel"
+                    data-slide="prev"
+                  >
+                    <i className="fa fa-angle-left" />
+                  </a>
+                  <a
+                    className="carousel-control right carousel-control-next"
+                    href="#myCarousel"
+                    data-slide="next"
+                  >
+                    <i className="fa fa-angle-right" />
+                  </a>
                 </div>
-                <a
-                  className="carousel-control left carousel-control-prev"
-                  href="#myCarousel"
-                  data-slide="prev"
-                >
-                  <i className="fa fa-angle-left" />
-                </a>
-                <a
-                  className="carousel-control right carousel-control-next"
-                  href="#myCarousel"
-                  data-slide="next"
-                >
-                  <i className="fa fa-angle-right" />
-                </a>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="loading-block">
+            <img src="home_loading.gif" width="20%" alt="Loading..." />
+          </div>
+        )}
       </React.Fragment>
     );
   }
